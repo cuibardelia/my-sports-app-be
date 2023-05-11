2a4234c87c6011a3ed831aed7ef9ec4f2ad0d5c5
Delia C <deliacuibar@gmail.com>
2023-05-11 23:26:45 +0300

    DB Work: - admin delete user - admin create trainer description
    NEXT:
    - routine schema

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const { protectAdmin } = require('../middleware/protect')
 const { deleteExercise } = require('../controllers/features');
+const { deleteUser, updateTrainer } = require('../controllers/userAdmin');
 
 router.use(protectAdmin);
 router.delete('/delete-exercise', deleteExercise);
+router.delete('/delete-user', deleteUser);
+router.post('/update-trainer', updateTrainer);
 
 module.exports = router;
